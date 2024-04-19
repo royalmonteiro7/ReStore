2a1c96d59ba28cb5fe4d26efa9092e74c65ca003
Royal <royalmonteiro7>
2024-04-19 14:47:55 +0530

    19/04/2024

---
+++ client/src/features/catalog/Catalog.tsx
@@ -0,0 +1,21 @@
+import { Product } from "../../models/product"
+import ProductList from "./ProductList";
+import { useEffect, useState } from "react";
+
+function Catalog() {
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/api/products')
+            .then(response => response.json())
+            .then(data => setProducts(data))
+    }, [])
+
+    return (
+        <>
+            <ProductList products={products} />
+        </>
+    )
+}
+
+export default Catalog
